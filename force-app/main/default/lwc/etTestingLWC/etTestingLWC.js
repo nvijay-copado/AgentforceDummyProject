76401f09618e86404e7512ff15e9d10040d9adef
Naman Vijay <namanvijay1997@gmail.com>
2026-08-27 03:19:57 +0000

    Copado Backup

---
+++ force-app/main/default/lwc/etTestingLWC/etTestingLWC.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class EtTestingLWC extends LightningElement {
+
+    connectedCallback(){
+        console.log('in connectecalback ds');
+    }
+}
---
+++ force-app/main/default/triggers/test7march.trigger
@@ -0,0 +1,3 @@
+trigger test7march on test7March__c (before insert) {
+
+}
